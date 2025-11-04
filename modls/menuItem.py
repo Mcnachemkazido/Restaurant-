15d2c30233b78509bba9ff3b129025a59d15a40d
MenachemKazido <agbg7172@gmail.com>
2025-11-04 17:44:43 +0200

    Create MenuItem Class

---
+++ modls/menuItem.py
@@ -0,0 +1,31 @@
+
+
+class MenuItem:
+    def __init__(self,name:str, price:float,category:str,available:bool=True):
+        self.name = name
+        self.price = price
+        self. category =  category
+        self.available = available
+
+    def get_info(self)->str:
+        return (f"name:{self.name}, price:{self.price },"
+                f" category:{self.category}")
+
+    def set_available(self,status):
+        if status == "is available":
+            self.available =  True
+        elif status == "sold out":
+            self.available = False
+        else:
+            raise ValueError("is available or sold out")
+
+    def is_available(self):
+        return self.available
+
+
+if __name__ == "__main__":
+    m = MenuItem("shuu",10,"drink")
+    print(m.get_info())
+    print(m.is_available())
+    m.set_available("sold out")
+    print(m.is_available())
